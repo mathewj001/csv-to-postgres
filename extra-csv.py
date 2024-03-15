8a79c096caf6aadba9f20ab12ef1f3210c39561b
mathewj001 <mathewjoseph.mec@gmail.com>
2024-03-15 15:10:50 +0530

    changes made

--- extra-csv.py
+++ extra-csv.py
@@ -18,7 +18,7 @@ table_name = 'test'
 
 
 df.to_sql(table_name, engine, if_exists='replace', index=False)
-print("Data imported into PostgreSQL database successfully.")
+print("Data imported to PostgreSQL database successfully.")
 
 
 engine.dispose()
